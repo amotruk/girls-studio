972522337fc7f3343057e1808c216af053b3a643
Anton Motruk <anton.motruk@tui.co.uk>
2021-11-07 15:12:37 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MastheadComponent } from './components/masthead/masthead.component';
+import { AboutComponent } from './components/about/about.component';
+import { TeamComponent } from './components/team/team.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ServicesComponent } from './components/services/services.component';
+import { HeadComponent } from './components/head/head.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MastheadComponent,
+    AboutComponent,
+    TeamComponent,
+    ContactComponent,
+    FooterComponent,
+    ServicesComponent,
+    HeadComponent
   ],
   imports: [
     BrowserModule,
